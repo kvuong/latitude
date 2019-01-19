dfdba6dfaadd167e4630a1e35647b7e1139ba519
Khoa Vuong <khoa.vuong@auspost.com.au>
2019-01-19 15:06:19 +1100

    Initial project setup.

---
+++ src/main/java/com/latitude/stockprice/StockPriceApplication.java
@@ -0,0 +1,11 @@
+package com.latitude.stockprice;
+
+import org.apache.log4j.Logger;
+
+public class StockPriceApplication {
+    final static Logger logger = Logger.getLogger(StockPriceApplication.class);
+
+    public static void main(final String[] args) {
+        logger.info("Hello World");
+    }
+}
