524b0a23275ee3bc53f1740b22d57cde4b7190f3
Khoa Vuong <khoa.vuong@auspost.com.au>
2019-01-20 16:22:09 +1100

    Update application with most efficient algorithm and provide readme on how to build and execute.

--- src/main/java/com/latitude/stockprice/StockPriceApplication.java
+++ src/main/java/com/latitude/stockprice/StockPriceApplication.java
@@ -1,11 +1,23 @@
 package com.latitude.stockprice;
 
-import org.apache.log4j.Logger;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class StockPriceApplication {
-    final static Logger logger = Logger.getLogger(StockPriceApplication.class);
+    private static final int FILE_PARAM = 0;
 
     public static void main(final String[] args) {
-        logger.info("Hello World");
+        try {
+            if (args.length == 0) {
+                throw new Exception("Please specify stock prices file.");
+            }
+
+            // Obtain list of stock prices from file passed in as parameter.
+            List<BigDecimal> stockPrices = StockPriceService.getStockPrices(args[FILE_PARAM]);
+
+            System.out.println(String.format("Maximum profit possible = $%s", StockPriceService.getMaxProfit(stockPrices)));
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
     }
 }
