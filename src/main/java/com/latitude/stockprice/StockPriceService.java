dcf8a175c6dd9ffb12fe56262178f587eb7c8757
Khoa Vuong <khoa.vuong@auspost.com.au>
2019-01-20 13:09:22 +1100

    Test most efficient algorithm with full sample test data.

--- src/main/java/com/latitude/stockprice/StockPriceService.java
+++ src/main/java/com/latitude/stockprice/StockPriceService.java
@@ -11,6 +11,34 @@ import java.util.stream.Stream;
 public class StockPriceService {
 
     /**
+     * Function that takes an array of stock prices and returns the best profit that could be made from 1 purchase and
+     * 1 sale of 1 Latitude Financial stock.
+     * @param stockPrices List of stock prices from yesterday.
+     * @return Maximum profit that could be made.
+     */
+    public static BigDecimal getMaxProfit(final List<BigDecimal> stockPrices) throws Exception {
+        BigDecimal largestGain = new BigDecimal(0);
+
+        try {
+            BigDecimal lowestPrice = stockPrices.get(0);
+
+            for (int index = 1; index < stockPrices.size(); index++) {
+                BigDecimal intervalGain = stockPrices.get(index).subtract(lowestPrice);
+                if (intervalGain.compareTo(largestGain) > 0) {
+                    largestGain = intervalGain;
+                }
+                if (stockPrices.get(index).compareTo(lowestPrice) < 0) {
+                    lowestPrice = stockPrices.get(index);
+                }
+            }
+        } catch (Exception error) {
+            throw new Exception("Unable to calculate max profit.", error);
+        }
+
+        return largestGain;
+    }
+
+    /**
      * Get stock prices from file.
      * @return List of stock prices.
      * @throws Exception
